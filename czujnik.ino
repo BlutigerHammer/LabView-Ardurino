2cfe4a63257dce453ac932fdfeece7448b7b34cd
BlutigerHammer <43138975+BlutigerHammer@users.noreply.github.com>
2019-11-18 22:35:37 +0100

    Add files via upload

---
+++ czujnik.ino
@@ -0,0 +1,26 @@
+#define trigPin 10
+#define echoPin 11
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  Serial.print(zmierz_odleglosc());
+  Serial.println("cm");
+  delay(500);
+}
+int zmierz_odleglosc(){
+  long czas, dystans;
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+  czas=pulseIn(echoPin,HIGH);
+  dystans=czas/58;
+  return dystans;
+}
